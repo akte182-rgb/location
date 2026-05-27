630fbe06ad6f32d5b3986f4fe7e57970dd23bbda
Cesar <akte182@gmail.com>
2026-05-27 17:53:59 -0600

    Initial

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+
+const app = express()
+
+const fs = require('fs')
+
+const path = require('path')
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+
+    res.sendFile(
+        path.join(__dirname, 'index.html')
+    )
+
+})
+
+
+app.post('/beacon', async (req, res) => {
+
+    console.log('Beacon recibido')
+
+    if (!fs.existsSync('logs')) {
+        fs.mkdirSync('logs')
+    }
+
+    const payload = {
+
+        timestamp: new Date().toISOString(),
+
+        ip:
+            req.headers['x-forwarded-for']
+            || req.socket.remoteAddress,
+
+        headers: {
+            userAgent: req.headers['user-agent'],
+            referer: req.headers['referer']
+        },
+
+        body: req.body
+    }
+
+    const filename =
+        `logs/${Date.now()}.json`
+
+    fs.writeFileSync(
+        filename,
+        JSON.stringify(payload, null, 2)
+    )
+
+    console.log('Log guardado')
+
+    res.sendStatus(204)
+})
+
+app.listen(3000, () => {
+    console.log('Servidor activo')
+})
